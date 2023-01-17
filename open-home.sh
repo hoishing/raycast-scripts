e5a580b69bae683b879099648df4e7b6e98efa70
Kelvin Ng <hoishing@gmail.com>
2023-01-17 16:35:10 +0800

    update open home

--- open-home.sh
+++ open-home.sh
@@ -2,14 +2,14 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title open py
+# @raycast.title open home
 # @raycast.mode silent
 # @raycast.packageName finder
 
 # Optional parameters:
-# @raycast.icon images/folder-py.svg
+# @raycast.icon images/folder-home.svg
 
 # Documentation:
-# @raycast.description Opens the py folder in the Finder.
+# @raycast.description Opens the home folder in the Finder.
 
-open ~/py
+open ~/
