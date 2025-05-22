55908f1ff41a03044f1a69834c87d44ea33c04c1
Kelvin Ng <hoishing@gmail.com>
2025-05-22 10:04:17 +0800

    add IOPaint script

---
+++ IOPaint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title IOPaint
+# @raycast.mode silent
+# @raycast.icon img/iopaint.svg
+# @raycast.description Opens the IOPaint app.
+
+cd ~/proj/io-paint
+.venv/bin/iopaint start --config iopaint-config.json
